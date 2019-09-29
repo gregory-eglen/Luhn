1d32070c0d3e4a23df45258159c6a329fbb7cce5
Gregory Eglen <eglen.g@gmail.com>
2019-09-29 18:33:45 +0000

    Refactored solution with mentor

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,36 +1,19 @@
 module Luhn
   def self.is_valid?(number)
-    new_digits = []
-    digits = number.to_s.split('')
-    
-    if digits.count > 16 or digits.count < 15
+    digits = number.digits
+
+    if digits.count != 16 && digits.count != 15
       return false
     end
 
-    digits.map! { |digit| digit.to_i }
-    digits[-2] *= 2
-    digits[-4] *= 2
-    digits[-6] *= 2
-    digits[-8] *= 2
-    digits[-10] *= 2
-    digits[-12] *= 2
-    digits[-14] *= 2
-
-    if digits.count == 16
-      digits[0] *= 2
+    digits.map!.with_index do |digit, i|
+      i.odd? && i > 0 ? digit*2 : digit
     end
 
-    digits.each do |digit|
-      if digit < 10
-        new_digits << digit
-      else
-        new_digits << digit - 9
-      end
+    digits.map! do |digit|
+      digit < 10 ? digit : digit - 9
     end
-    if new_digits.sum % 10 == 0
-      return true
-    else
-      return false
-    end
+       
+    digits.sum % 10 == 0
   end
 end
